d44e30c01f49d0ece1965c2cf7d1a68076b5c7aa
jeffbcole <jeffbcolemail@gmail.com>
2017-12-08 09:25:00 -0800

    Redirect for mobile users

--- settings.js
+++ settings.js
@@ -56,3 +56,21 @@ function GetStatisticString(statistic) {
 function SetStatistic(statistic, value) {
 	window.localStorage.setItem(statistic, value);
 }
+
+function redirectToAppStore() {
+	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+	if (/windows phone/i.test(userAgent)) {
+		return true;
+	}
+  
+	if (/android/i.test(userAgent)) {
+		return true;
+	}
+  
+	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+		return true;
+	}
+  
+	return false;
+}

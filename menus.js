94f2acf564b2b2689f5570b90c545bcfb074ad23
JeffCole Mac <jeffbcolemail@gmail.com>
2019-05-14 13:45:17 -0700

    Link to pinochle
    adding link to pinochle

--- menus.js
+++ menus.js
@@ -999,7 +999,7 @@ function play_more_games_close_click() {
 	var el = document.getElementById('play_more_games_menu');
 	with(el.style) {
 		WebkitTransition = MozTransition = OTransition = msTransition = "0.4s ease-in";
-		bottom = "-200pt";
+		bottom = "-260pt";
 		opacity = 0;
 		pointerEvents = "none";
 	}
